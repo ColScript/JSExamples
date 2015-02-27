ecf766d8250c190746a6c26613d6b41f5f9daae6
Luis Rogelio Hernández López <luis.hlopez@outlook.com>
2015-02-27 11:55:23 -0600

    Update hash.js

--- hash.js
+++ hash.js
@@ -1,6 +1,5 @@
 var crypto		=	require('crypto');
-var hash		=	crypto.createHash('md5');
 var plainText	=	'Hashing is OP';
-var encrypted	=	hash.update(plainText, 'utf8').digest('hex');
+var encrypted	=	crypto.createHash('md5').update(plainText, 'utf8').digest('hex');
 console.log("\nOriginal text: " + plainText + "\n");
 console.log("Encrypted text: " + encrypted +"\n");
